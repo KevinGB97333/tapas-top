9944ee29475958b159115f78d6a03312b16f908c
KevinGB97333 <kevinguarnizobetancourt@gmail.com>
2022-10-26 18:10:10 +0200

    Add login code with view model and app name with animation on fragment initial

--- TapasTop/app/src/main/java/es/upm/isii/tapastop/LoginTabFragment.kt
+++ TapasTop/app/src/main/java/es/upm/isii/tapastop/LoginTabFragment.kt
@@ -8,10 +8,14 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import es.upm.isii.tapastop.databinding.FragmentLoginTabBinding
+import es.upm.isii.tapastop.model.UserViewModel
 
 class LoginTabFragment : Fragment() {
 
@@ -19,11 +23,20 @@ class LoginTabFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    private lateinit var username : TextInputLayout
-    private lateinit var password : TextInputLayout
+    private val sharedViewModel : UserViewModel by activityViewModels()
+    //TextInputLayout
+    private lateinit var usernameTL : TextInputLayout
+    private lateinit var passwordTL : TextInputLayout
+    //EditText
+    private lateinit var usernameET : TextInputEditText
+    private lateinit var passwordET : TextInputEditText
+
     private lateinit var forgotPassword : TextView
     private lateinit var loginBtn : Button
 
+
+
+    //Animations
     private lateinit var usernameAnimation : Animation
     private lateinit var passwordAnimation : Animation
     private lateinit var loginAnimation : Animation
@@ -35,18 +48,20 @@ class LoginTabFragment : Fragment() {
     ): View? {
         _binding = FragmentLoginTabBinding.inflate(inflater,container,false)
         val root = binding.root
-        username = binding.loginUsername
-        password = binding.loginPassword
+        usernameTL = binding.loginUsername
+        passwordTL = binding.loginPassword
         forgotPassword = binding.forgotPassword
         loginBtn = binding.btnLogin
 
+        usernameET = binding.loginUsernameText
+        passwordET = binding.loginPasswordText
         //Animations
         usernameAnimation = AnimationUtils.loadAnimation(activity,R.anim.start_username_field_anim)
         passwordAnimation = AnimationUtils.loadAnimation(activity, R.anim.start_password_field_anim)
         loginAnimation = AnimationUtils.loadAnimation(activity, R.anim.start_button_anim)
 
-        username.startAnimation(usernameAnimation)
-        password.startAnimation(passwordAnimation)
+        usernameTL.startAnimation(usernameAnimation)
+        passwordTL.startAnimation(passwordAnimation)
         forgotPassword.startAnimation(passwordAnimation)
         loginBtn.startAnimation(loginAnimation)
 
@@ -57,8 +72,45 @@ class LoginTabFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
             loginBtn.setOnClickListener{
-                findNavController().navigate(R.id.action_initialFragment_to_mainMenuFragment)
+                validateFields()
+            }
+            usernameET.addTextChangedListener{
+                usernameET.addTextChangedListener {
+                    if (it.toString().isBlank()) {
+                        usernameTL.error = getString(R.string.empty_field_error_msg)
+                    } else {
+                        usernameTL.error = null
+                    }
+                }
+            }
+            passwordET.addTextChangedListener {
+                if (it.toString().isBlank()) {
+                    passwordTL.error = getString(R.string.empty_field_error_msg)
+                } else {
+                    passwordTL.error = null
+                }
             }
         }
+    }
+    private fun validateFields(){
+        val username = usernameET.text.toString().trim()
+        val password = passwordET.text.toString().trim()
+        val emptyFields = checkEmptyFields(username, password)
+        if(!emptyFields){
+            sharedViewModel.setUsername(username)
+            findNavController().navigate(R.id.action_initialFragment_to_mainMenuFragment)
+        }
+    }
+    private fun checkEmptyFields(username : String , password : String) : Boolean{
+        var emptyField = false
+        if(username == ""){
+            usernameTL.error = getString(R.string.empty_field_error_msg)
+            emptyField = true
+        }
+        if(password == ""){
+            passwordTL.error = getString(R.string.empty_field_error_msg)
+            emptyField = true
+        }
+        return emptyField
     }
 }
